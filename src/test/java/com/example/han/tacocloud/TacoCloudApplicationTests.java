30db0b9797801310f9eb1294948336b666abf9bc
HaNiks <haniks2899@gmail.com>
2022-07-01 22:54:08 +0300

    init(taco): start project

---
+++ src/test/java/com/example/han/tacocloud/TacoCloudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.han.tacocloud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TacoCloudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
